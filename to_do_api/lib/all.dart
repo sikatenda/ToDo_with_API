706f14715048b24f980c019747f859501a35f548
Jacques Feruzi <jferuzi019@gmail.com>
2025-10-11 09:43:03 +0200

    all
    I create a page that will display all the records

---
+++ to_do_api/lib/all.dart
@@ -0,0 +1,31 @@
+// ths page will display the record from the API
+import 'package:flutter/material.dart';
+
+class MyAll extends StatefulWidget {
+  const MyAll({super.key});
+
+  @override
+  State<MyAll> createState() => _MyAllState();
+}
+
+class _MyAllState extends State<MyAll> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Welcom'),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
