49a474bd3535dcaf055a19447d0fcca47e1e6ad3
Jacques Feruzi <jferuzi019@gmail.com>
2025-11-13 11:57:27 +0200

    model page
    dome with the model page

--- to_do_api/lib/model.dart
+++ to_do_api/lib/model.dart
@@ -1,2 +1,18 @@
+// This contains the model of my API response
 
-// This contains the model of my API response 
+class Tasks {
+  late final int id;
+  late final String name;
+
+  Tasks({required this.id, required this.name});
+
+  factory Tasks.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {'id': int id, 'name': String name} => Tasks(
+          id: id,
+          name: name,
+        ),
+      _ => throw const FormatException('Failed to load album.'),
+    };
+  }
+}
